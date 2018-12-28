3124150e6344e53f484f5a51cb0d3d3325fe5ef5
olga.pigareva <pigareva@billiger-mietwagen.de>
2018-12-28 14:34:54 +0100

    feat: implement react table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,61 @@
 import React, { Component } from 'react';
+import ReactTable from "react-table";
 
+import 'react-table/react-table.css'
 import './App.css';
 import logo from './logo.png'
 
 class App extends Component {
+  componentDidMount() {
+
+  }
+
   render() {
+    const data = [{
+      page: 'serp',
+      date: '10.10.2000',
+      text: 'texxt dm;gnfbgbmgb'
+    },
+      {
+        page: 'select',
+        date: '11.11.2000',
+        text: 'texxt dm;gnfbgbmgb'
+      },
+      {
+        page: 'serp',
+        date: '12.12.2012',
+        text: 'texxt dm;gnfbgbmcbvbvnbvnngb'
+      }
+    ];
+
+    const columns = [{
+
+      accessor: 'page',
+      className: 'Column PageColumn'
+    }, {
+
+      accessor: 'date',
+      className: 'Column DateColumn'
+    }, {
+
+      accessor: 'text',
+      className: 'Column TextColumn'
+    },
+    ];
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} alt="logo" />
-          FeeLoo
+          Unsere Kunden sagen:
         </header>
         <main className="App-main">
-          <table className="Table">
-            <tr className="TableRow">
-              <td className="PageColumn Column">
-                serp
-              </td>
-              <td className="DateColumn Column">
-10.10.2018
-              </td>
-              <td className="TextColumn Column">
-fdkbhvdflkbgfgkb nfbfgbfg
-              </td>
-            </tr>
-
-            <tr className="TableRow">
-              <td className="PageColumn Column">
-                <span>
-                  serp
-                </span>
-
-              </td>
-              <td className="DateColumn Column">
-                10.10.2018
-              </td>
-              <td className="TextColumn Column">
-                fdkbhvdflkbgfgkb nfbfgbfg
-              </td>
-            </tr>
-
-            <tr className="TableRow">
-              <td className="PageColumn Column">
-                serp
-              </td>
-              <td className="DateColumn Column">
-                10.10.2018
-              </td>
-              <td className="TextColumn Column">
-                fdkbhvdflkbgfgkb nfbfgbfg
-              </td>
-            </tr>
-
-          </table>
+          <ReactTable
+            data={data}
+            columns={columns}
+            showPagination={false}
+            defaultPageSize={3}
+          />
         </main>
         <footer className="App-footer">
 
