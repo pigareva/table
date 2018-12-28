b250e6ecbd09c08694c30ed7e8a25d33a0b3645f
olga.pigareva <pigareva@billiger-mietwagen.de>
2018-12-28 17:38:01 +0100

    feat: fetch and show feedback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,31 +5,55 @@ import 'react-table/react-table.css'
 import './App.css';
 import logo from './logo.png'
 
-class App extends Component {
-  componentDidMount() {
+const defaultPageSize = 3;
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.selectFeedback = this.selectFeedback.bind(this);
+    this.startTime = this.startTime.bind(this);
+
+    this.state = {
+      data: null,
+      feedbacks: null,
+    }
+  }
+  async componentDidMount() {
+    const res = await fetch('https://api.billiger-mietwagen.de/v1/survey');
+    const json = await res.json();
+    const array  = json.survey.filter((feedback) => feedback.text && (feedback.text.length > 20));
+    const feedbacks = array.map(item => ({
+      page: item.page,
+      date: item.date,
+      text: item.text.toString()
+    }))
+      .sort((a,b) => a.date - b.date);
+
+    this.setState({ feedbacks });
+    this.startTime();
+  }
+
+  selectFeedback() {
+    this.setState({ data: this.state.feedbacks.slice(this.setNumber, this.setNumber + defaultPageSize )});
+    this.setNumber +=defaultPageSize;
+    if (this.state.feedbacks.length - this.setNumber < 3) {
+      this.setNumber = 0;
+    }
+  }
+
+  startTime() {
+    this.setNumber = 0;
+    this.timerID = setInterval(
+      () => this.selectFeedback(),
+      10000,
+    );
   }
 
   render() {
-    const data = [{
-      page: 'serp',
-      date: '10.10.2000',
-      text: 'texxt dm;gnfbgbmgb'
-    },
-      {
-        page: 'select',
-        date: '11.11.2000',
-        text: 'texxt dm;gnfbgbmgb'
-      },
-      {
-        page: 'serp',
-        date: '12.12.2012',
-        text: 'texxt dm;gnfbgbmcbvbvnbvnngb'
-      }
-    ];
+    const data = this.state.data;
 
     const columns = [{
-
       accessor: 'page',
       className: 'Column PageColumn'
     }, {
@@ -39,24 +63,31 @@ class App extends Component {
     }, {
 
       accessor: 'text',
-      className: 'Column TextColumn'
+      className: 'Column TextColumn',
+      Cell: props => <span dangerouslySetInnerHTML={{ __html:props.value }} />
     },
     ];
+
+    const table = <ReactTable
+      data={data}
+      columns={columns}
+      showPagination={false}
+      defaultPageSize={defaultPageSize}
+    />;
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} alt="logo" />
           Unsere Kunden sagen:
+          <img src={logo} alt="logo" />
         </header>
+
         <main className="App-main">
-          <ReactTable
-            data={data}
-            columns={columns}
-            showPagination={false}
-            defaultPageSize={3}
-          />
+          {
+            data ? table : <div>Loading...</div>
+          }
         </main>
+
         <footer className="App-footer">
 
         </footer>
